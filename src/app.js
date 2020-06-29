fc1508c594cf665c39cd0437cfc635fdd2c8ba1b
Shai <dev@ashato.co>
2020-06-30 01:27:32 +0300

    example 2

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
-const preload = function() {
-	this.load.setBaseURL('http://labs.phaser.io');
+// base config
+const BASE_URL = 'http://localhost:8080/src/';
 
-	this.load.image('sky', 'assets/skies/space3.png');
-	this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-	this.load.image('red', 'assets/particles/red.png');
+//
+const HERO_SLIDING = -1;
+const HERO_IDLE = -2;
+const HERO_DEAD = -3;
+
+const preload = function() {
+	this.load.setBaseURL(BASE_URL);
+
+	this.load.image('hero', 'assets/hero/_PNG/3_ORK/IDLE/IDLE_000.png');
 }
 
 const create = function() {
-	this.add.image(400, 300, 'sky');
+	//this.add.image(100, 100, 'hero');
 
+	/*
 	var particles = this.add.particles('red');
 
 	var emitter = particles.createEmitter({
@@ -16,14 +23,16 @@ const create = function() {
 		scale: { start: 1, end: 0 },
 		blendMode: 'ADD'
 	});
+	*/
+	let logo = this.physics.add.image(50, 50, 'hero');
 
-	var logo = this.physics.add.image(400, 100, 'logo');
-
-	logo.setVelocity(200, 400);
+	logo.setScale(0.3);
+	logo.setVelocity(10, 10);
 	logo.setBounce(1,1);
 	logo.setCollideWorldBounds(true);
+	
+	//emitter.startFollow(logo);
 
-	emitter.startFollow(logo);
 };
 
 let config = {
